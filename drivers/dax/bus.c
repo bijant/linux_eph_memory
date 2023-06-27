9f0e0b3deeb1825ad113d7aebe7056dcf2bbc5f9
Ira Weiny <ira.weiny@intel.com>
2023-06-27 12:40:30 -0700

    cxl/region: Add DC DAX region support
    DC DAX regions must allow memory to be added or removed dynamically.
    In addition to the quantity of memory available the,
    location of the memory within a DC partition is dynamic, based on the
    extents offered by a device.  CXL DAX regions must accommodate the
    dynamic movement of this memory in the management of DAX regions and devices.
    
    Introduce the concept of a dynamic DAX region. Introduce
    create_dynamic_ram_a_region() sysfs entry to create such regions.
    Special case DC-capable regions to create a 0 sized seed DAX device
    to maintain compatibility which requires a default DAX device to hold a
    region reference.
    
    Indicate 0 byte available capacity until such time that capacity is
    added.
    
    Dynamic regions complicate the range mapping of dax devices.  There is no
    known use case for range mapping on dynamic regions.  Avoid the
    complication by preventing range mapping of dax devices on dynamic
    regions.
    
    Interleaving is deferred for now.  Add checks.
    
    Based on an original patch by Navneet Singh.
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    
    ---
    Changes:
    [anisa: rebase]
    [anisa: change "sparse" naming conventions and to "dynamic"]

--- drivers/dax/bus.c
+++ drivers/dax/bus.c
@@ -181,6 +181,11 @@ static bool is_static(struct dax_region *dax_region)
 	return (dax_region->res.flags & IORESOURCE_DAX_STATIC) != 0;
 }
 
+static bool is_dynamic(struct dax_region *dax_region)
+{
+	return (dax_region->res.flags & IORESOURCE_DAX_DCD) != 0;
+}
+
 bool static_dev_dax(struct dev_dax *dev_dax)
 {
 	return is_static(dev_dax->region);
@@ -303,6 +308,9 @@ static unsigned long long dax_region_avail_size(struct dax_region *dax_region)
 	struct resource *res;
 
 	lockdep_assert_held(&dax_region_rwsem);
+
+	if (is_dynamic(dax_region))
+		return 0;
 
 	for_each_dax_region_resource(dax_region, res)
 		size -= resource_size(res);
@@ -1388,6 +1396,8 @@ static umode_t dev_dax_visible(struct kobject *kobj, struct attribute *a, int n)
 	if (a == &dev_attr_numa_node.attr && !IS_ENABLED(CONFIG_NUMA))
 		return 0;
 	if (a == &dev_attr_mapping.attr && is_static(dax_region))
+		return 0;
+	if (a == &dev_attr_mapping.attr && is_dynamic(dax_region))
 		return 0;
 	if ((a == &dev_attr_align.attr ||
 	     a == &dev_attr_size.attr) && is_static(dax_region))
